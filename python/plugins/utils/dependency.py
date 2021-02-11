8256400e7b6ee79e66cdf2d49cc603f66521bc40
오혜성 <surgachsurgach@gmail.com>
2021-02-11 21:07:40 +0900

    Fix Typo

--- python/plugins/utils/dependency.py
+++ python/plugins/utils/dependency.py
@@ -1,8 +1,5 @@
 from functools import reduce
 
-
-# TODO: See airflow.utils.helpers
-# TODO: How to give type hint on this function?
 def sequential_dependency(xs: list, ys: list = None):
     if ys is None:
         return reduce(lambda x, y: x >> y, xs)
